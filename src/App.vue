ad37c53fec6becf1c7c0909bf697fa1592acd5a4
Simon <kartsims@gmail.com>
2021-12-28 18:54:17 +0100

    test font

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <canvas id="canvas"></canvas>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import opentype from 'opentype'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    async loadFont() {
+      try {
+        const font = await opentype.load('momcake.otf')
+              // Now let's display it on a canvas with id "canvas"
+        const ctx = document.getElementById('canvas').getContext('2d');
+
+        // Construct a Path object containing the letter shapes of the given text.
+        // The other parameters are x, y and fontSize.
+        // Note that y is the position of the baseline.
+        const path = font.getPath('Hello, World!', 0, 150, 72);
+        console.log('path', path)
+
+        // If you just want to draw the text you can also use font.draw(ctx, text, x, y, fontSize).
+        path.draw(ctx);
+      } catch(err) {
+        console.log('err', err);
+      }
+    }
   }
 }
 </script>
